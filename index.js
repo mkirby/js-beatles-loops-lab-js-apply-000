5c2987cfd62e8195b812937bc1c9daf761d8a371
Matthew Kirby <matthew.robbins.kirby@gmail.com>
2020-06-26 17:14:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  let whoPlaysWhat = [];
+  for (var i = 0; i < musicians.length; i++) {
+    whoPlaysWhat.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return whoPlaysWhat;
+}
